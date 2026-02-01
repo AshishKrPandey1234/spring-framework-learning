0fba33b51585401bd7b15af32b23511bf3617b4d
Ashish Kumar Pandey <akpandey369936@gmail.com>
2026-02-01 20:06:57 +0530

    Use @Component and @Value annotations

---
+++ src/main/java/com/example/componentscan/Employee.java
@@ -0,0 +1,62 @@
+package com.example.componentscan;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component("employee")
+public class Employee {
+    @Value("101")
+    private int employeeId;
+
+    @Value("Ashish")
+    private String firstName;
+
+    @Value("Pandey")
+    private String lastName;
+
+    @Value("#{58900*12}")
+    private double salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
